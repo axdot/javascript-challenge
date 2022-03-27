1a27f7820c154559c68c3baf52d85a4fb4d38c78
Chris <kimballc3@yahoo.com>
2022-03-27 16:27:09 -0500

    questions input into array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,31 +3,85 @@ document.getElementById("answer-choices").innerHTML = "";
 document.getElementById("correct").innerHTML = "";
 document.getElementById("wrong").innerHTML = "";
 
+let question = document.querySelector("#questions")
+let answer1 = document.querySelector("#first-choice")
+let answer2 = document.querySelector("#second-choice")
+let answer3 = document.querySelector("#third-choice")
+let answer4 = document.querySelector("#fourth-choice")
+
+const questions = [{
+    questionOne: 'Commonly used data types DO not include:',
+    answerOne: 'strings',
+    answerTwo: 'booleans',
+    answerThree: 'alerts',
+    answerFour: 'numbers',
+    // answer: 'answerThree'
+  },
+
+  {
+    questionTwo: 'The condition in an if/else statement is enclosed with _______.',
+    answerOne: 'quotes',
+    answerTwo: ' curly brackets', 
+    answerThree: ' parentheses',
+    answerFour: 'square brackets',
+    // answer: 'answerThree',
+  },
+
+  {
+    questionThree: 'Arrays in JavaScript can be used to store _______.',
+    answerOne: 'numbers and strings',
+    answerTwo: 'other arrays',
+    answerThree: 'booleans',
+    answerFour: 'all of the above',
+    // answer: 'answerFour',
+  },
+
+  {
+    questionFour: 'String values must be enclosed within _______ when being assigned to variables.',
+    answerOne: 'commas',
+    answerTwo: 'curly brackets',
+    answerThree: 'quotes',
+    answerFour:  'parentheses',
+    // answer: 'answerThree',
+  },
+
+  {
+    questionFive: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+    answerOne: 'JavaScript',
+    answerTwo: 'terminal/bash',
+    answerThree: 'for loops',
+    answerFour: 'console.log',
+    // answer: answerFour
+  }
+];
+
+// This is where I am at.
+let quiz = function() {
+  let question1 = questions[0].questionOne;
+  let answerA = questions[0].answerOne;
+  let answerB = questions[0].answerTwo;
+  let answerC = questions[0].answerThree;
+  let answerD = questions[0].answerFour;
+
+};
+// 
+
 document.getElementById("start-quiz-button").onclick = function(){
-console.log("hello")
-
-
+quiz();
 }
 
-var questions =[
-
-]
 
 
 
 
-
-
-
-
-
+// const obj = {name: "John", age: 30, city: "New York"};
+// const myJSON = JSON.stringify(obj.name);
+// document.getElementById("demo").innerHTML = myJSON;
 
 
 //replace elements with JS
 
 // pseudocode (steps from Will)
-
-// an array of questions and answers
 
 // questions and answers tied together in an object
 
